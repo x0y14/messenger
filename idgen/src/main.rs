e406333c7fc08d547bdba569df6f8f3ae702fd5d
x0y14 <ya01ma08@gmail.com>
2022-06-14 17:33:12 +0900

    仮置き

--- idgen/src/main.rs
+++ idgen/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let root = warp::path::end().map(|| "id-gen");
+    let ping = warp::path("ping").map(|| "ping");
+
+    let revision = warp::path("revision").map(|| "Hello, World!");
+    let id = warp::path("id").map(|| "id");
+
+    warp::serve(root
+        .or(ping)
+        .or(revision)
+        .or(id))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
