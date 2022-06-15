1f6a95de341161f0fbbab4bc4b0c912c0de5af00
x0y14 <ya01ma08@gmail.com>
2022-06-15 11:59:15 +0900

    若干わかりずらかったのでwarp->actix_webに。

--- idgen/src/main.rs
+++ idgen/src/main.rs
@@ -1,21 +1,46 @@
-use warp::Filter;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use nanoid::nanoid;
+use snowflake::SnowflakeIdGenerator;
+use std::{sync::Mutex};
+use once_cell::sync::Lazy;
 
-#[tokio::main]
-async fn main() {
-    let addr = 3030;
 
-    println!("id-gen listening on: {}", addr);
+static SNOWFLAKE_ID_GENERATOR: Lazy<Mutex<SnowflakeIdGenerator>> = Lazy::new(|| {
+    let idgen = SnowflakeIdGenerator::new(1, 1);
+    Mutex::new(idgen)
+});
 
-    let root = warp::path::end().map(|| "id-gen");
-    let ping = warp::path("ping").map(|| "ping");
 
-    let revision = warp::path("revision").map(|| "Hello, World!");
-    let id = warp::path("id").map(|| "id");
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("this is idgen")
+}
 
-    warp::serve(root
-        .or(ping)
-        .or(revision)
-        .or(id))
-        .run(([0, 0, 0, 0], addr))
-        .await;
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+async fn generate_revision() -> impl Responder {
+    let rev = SNOWFLAKE_ID_GENERATOR.lock().unwrap().generate();
+    HttpResponse::Ok().body(rev.to_string())
+}
+
+async fn generate_id() -> impl Responder {
+    let id = nanoid!(20);
+    HttpResponse::Ok().body(id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/ping", web::get().to(ping))
+            .route("/revision", web::get().to(generate_revision))
+            .route("/id", web::get().to(generate_id),
+            )
+    })
+        .bind(("0.0.0.0", 3030))?
+        .run()
+        .await
 }
