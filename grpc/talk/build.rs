8f79b15ae10e3971cba79b43933c1ebde11ade95
x0y14 <ya01ma08@gmail.com>
2022-06-17 17:07:29 +0900

    :white_check_mark: add talk files

---
+++ grpc/talk/build.rs
@@ -0,0 +1,6 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::compile_protos("../proto/supervisor_service.proto")?;
+    tonic_build::compile_protos("../proto/talk_service.proto")?;
+    tonic_build::compile_protos("../proto/types.proto")?;
+    Ok(())
+}
