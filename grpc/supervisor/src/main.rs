deb035c5983088200bf712b7026b8fc2e55ffe66
x0y14 <ya01ma08@gmail.com>
2022-06-23 21:47:09 +0900

    :white_check_mark: supervisorのアカウント生成のみ実装。

--- grpc/supervisor/src/main.rs
+++ grpc/supervisor/src/main.rs
@@ -1,11 +1,16 @@
 use connector::db::{accounts};
-use connector::idgen::{get_new_id};
+use connector::idgen::{get_new_id, get_new_revision};
 mod lib;
 
 use tonic::{transport::Server, Request, Response, Status};
 use connector::db;
-use connector::db::accounts::get_multiple_account_with_email;
-use connector::db::models::{InputInsertAccount};
+
+use diesel::{PgConnection, r2d2};
+use diesel::r2d2::{ConnectionManager};
+use dotenv::{dotenv, var};
+
+use crate::db::models::{InputInsertAccount, };
+use crate::db::accounts::{get_multiple_account_with_email, get_single_account, insert_single_account, update_single_account};
 
 pub mod supervisor_service { tonic::include_proto!("supervisor_service"); }
 use supervisor_service::{
@@ -38,34 +43,67 @@ impl SupervisorService for SupervisorServiceProvider {
     }
 
     async fn create_account(&self, request: Request<CreateAccountRequest>) -> Result<Response<CreateAccountReply>, Status> {
-        // リクエストに含まれるemailをもつアカウントが存在しないことを確認
-        let acc: types::Account = request.into_inner().account.unwrap();
+        let acc: Account = request.into_inner().account.unwrap();
         
+        // リクエストに含まれるemailをもつアカウントが存在しないことを確認
         let accounts_ = get_multiple_account_with_email(
-            self.pool.clone(), acc.email).unwrap();
+            self.pool.clone(), acc.email.clone()).unwrap();
         // 存在している
         if accounts_.len() >= 1 {
             Err(Status::already_exists("this email is already used."))?
         }
+        // todo : usernameが重複してないか。
 
         // id生成
         let user_id = get_new_id().await.unwrap();
+        let revision = get_new_revision().await.unwrap();
+        
+        let res = insert_single_account(self.pool.clone(), InputInsertAccount {
+            id: &user_id,
+            email: &acc.email,
+            username: if acc.username == "" {None} else {Some(&acc.username)},
+        }).expect("failed to create account");
 
-        // let new_account = InputInsertAccount {
-        //     id: &user_id,
-        //     email: &acc.email,
-        //     username: acc.username
-        // }
+        Ok(Response::new(CreateAccountReply{ revision: revision.parse().unwrap(), account: Some(types::Account{
+            id: res.id,
+            email: res.email,
+            username: if res.username == None { "".to_string() } else { res.username.unwrap() },
+            created_at: res.created_at.to_rfc3339(),
+            updated_at: res.updated_at.to_rfc3339()
+        }) }))
+    }
 
-        // Ok(Response::new(()))
+    async fn create_profile(&self, request: Request<CreateProfileRequest>) -> Result<Response<CreateProfileReply>, Status> {
+        todo!()
+    }
 
-        Err(Status::unimplemented(""))
+    async fn record_operation(&self, request: Request<RecordOperationRequest>) -> Result<Response<RecordOperationReply>, Status> {
+        todo!()
     }
 }
 
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     // let provider = SupervisorServiceProvider{}
-// }
-fn main() {}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50060".parse().unwrap();
+
+    // 接続
+    dotenv().ok();
+    let db_url = var("DATABASE_URL").expect("failed to load DATABASE_URL");
+
+    let manager = ConnectionManager::<PgConnection>::new(db_url);
+    let pool: db::Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("failed to create pool");
+
+    let provider = SupervisorServiceProvider{pool: pool};
+
+    println!("supervisor service listening on {} !", addr);
+
+    Server::builder()
+        .add_service(SupervisorServiceServer::new(provider))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
